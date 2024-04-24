2eeef14def4035120f651341c00babd18a5f591b
André Lazenga <134404381+lazenga@users.noreply.github.com>
2024-04-24 16:30:18 +0100

    added gitignore and implemented runtests script

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,38 @@
+#!/bin/bash
 
+green='\033[0;32m'
+red='\033[0;31m'
+end_color='\033[0m'
 
+temp_dir="temp-output"
 
+if [ -d "$temp_dir" ]; then
+    rm -r "$temp_dir"
+fi
+
+mkdir "$temp_dir"
+
+for i in {1..9}; do
+    # Input and expected output files
+    tests_path="tests"
+    input_file="$tests_path/test-0$i.txt"
+    expected_output_file="$tests_path/test-0$i.out"
+    temp_output_file="$temp_dir/test-0$i.out"
+
+    output=$(python3 pipe.py < "$input_file")
+    echo "$output" > "$temp_output_file"
+
+    # Compare the output with the expected output
+    echo "---------------"
+    if [ "$output" = "$(cat $expected_output_file)" ]; then
+        echo -e "${green}Test $i: PASSED${end_color}"
+    else
+        echo -e "${red}Test $i: FAILED${end_color}"
+        echo -e "${red}Expected output:${end_color}"
+        cat "$expected_output_file"
+        echo -e "${red}Actual output:${end_color}"
+        echo "$output"
+    fi
+done
+
+echo "---------------"
